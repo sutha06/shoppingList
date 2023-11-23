38aef5379f60cba7edc52915caad0d122043bb80
sutha06 <suthakaran.siva0601@gmail.com>
2023-11-22 23:06:42 -0500

    Item detail model file made

---
+++ app/src/main/java/ca/suthakaran/assignment3/ui/model/ItemDetailsModel.kt
@@ -0,0 +1,24 @@
+package ca.suthakaran.assignment3.ui.model
+
+import ca.suthakaran.assignment3.domain.Item
+import ca.suthakaran.assignment3.ui.common.currencyformat
+
+data class ItemDetailsModel(
+    val id: Int,
+    val name: String,
+    val price: String,
+    val quantity: Int
+){
+    constructor(item: Item): this(
+        id = item.id,
+        name = item.name,
+        price = currencyformat(item.price),
+        quantity = item.quantity
+    )
+
+    constructor(): this(Item())
+}
+
+fun Item.toItemDetailsModel() = ItemDetailsModel(this)
+
+
