3d4dc53bb7fa9a5e7d62ed7cf7082f1ec578cbd0
sutha06 <suthakaran.siva0601@gmail.com>
2023-11-22 22:58:56 -0500

    ListItemModel file made

---
+++ app/src/main/java/ca/suthakaran/assignment3/ui/model/ListItemModel.kt
@@ -0,0 +1,26 @@
+package ca.suthakaran.assignment3.ui.model
+
+import ca.suthakaran.assignment3.domain.Item
+import ca.suthakaran.assignment3.ui.common.currencyformat
+
+data class ListItemModel(
+    val id: Int,
+    val name: String,
+    val price: String,
+    val quantity: Int,
+    val selected: Boolean
+){
+    constructor(item: Item): this(
+        id = item.id,
+        name = item.name,
+        price = currencyformat(item.price),
+        quantity = item.quantity,
+        selected = item.selected
+    )
+
+    constructor(): this(Item())
+}
+
+fun Item.toListItemModel() = ListItemModel(this)
+
+
