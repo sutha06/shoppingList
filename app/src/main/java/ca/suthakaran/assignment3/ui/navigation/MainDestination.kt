2836a27e35aa578a01cea173309798097aed064f
sutha06 <suthakaran.siva0601@gmail.com>
2023-11-23 00:11:44 -0500

    MainDestination file complete

---
+++ app/src/main/java/ca/suthakaran/assignment3/ui/navigation/MainDestination.kt
@@ -0,0 +1,8 @@
+package ca.suthakaran.assignment3.ui.navigation
+
+import ca.suthakaran.assignment3.R
+
+object MainDestination : NavigationDestination {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
