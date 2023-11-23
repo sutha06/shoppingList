d72b0eb55c48458ce2792006a27c1a431b56555b
sutha06 <suthakaran.siva0601@gmail.com>
2023-11-23 00:13:00 -0500

    nothing was done to main activity, but dimens.xml, strings.xml and Theme.kt were adjusted

--- app/src/main/java/ca/suthakaran/assignment3/MainActivity.kt
+++ app/src/main/java/ca/suthakaran/assignment3/MainActivity.kt
@@ -10,13 +10,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import ca.suthakaran.assignment3.ui.theme.Assignment3Theme
+import ca.suthakaran.assignment3.ui.theme.ShoppingTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Assignment3Theme {
+            ShoppingTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
@@ -40,7 +40,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    Assignment3Theme {
+    ShoppingTheme {
         Greeting("Android")
     }
 }
