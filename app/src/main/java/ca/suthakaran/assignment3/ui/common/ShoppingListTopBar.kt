d6e7d3fbfbc55ebe696a75e96ddb80ba3c37be75
sutha06 <suthakaran.siva0601@gmail.com>
2023-11-22 22:44:49 -0500

    ShoppingListTopBar file made

---
+++ app/src/main/java/ca/suthakaran/assignment3/ui/common/ShoppingListTopBar.kt
@@ -0,0 +1,44 @@
+package ca.suthakaran.assignment3.ui.common
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import ca.suthakaran.assignment3.R
+
+
+/**
+ * App bar to display title and conditionally display the back navigation.
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun InventoryTopAppBar(
+    title: String,
+    canNavigateBack: Boolean,
+    modifier: Modifier = Modifier,
+    scrollBehavior: TopAppBarScrollBehavior? = null,
+    navigateUp: () -> Unit = {}
+) {
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
