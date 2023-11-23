07858745b125a755612aae54305228c5763538be
sutha06 <suthakaran.siva0601@gmail.com>
2023-11-22 20:51:01 -0500

    RepositoryModule file made

---
+++ app/src/main/java/ca/suthakaran/assignment3/data/repository/RepositoryModule.kt
@@ -0,0 +1,14 @@
+package ca.suthakaran.assignment3.data.repository
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class RepositoryModule {
+
+    @Binds
+    abstract fun itemsRepository(repository: LocalItemsRepository): ItemsRepository
+}
